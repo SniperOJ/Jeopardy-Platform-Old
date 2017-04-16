88c74ca00b00bc6a40b960e34dfd157aad5745e5
WangYihang <wangyihanger@gmail.com>
2017-04-16 14:48:23 +0000

    modify alert

--- application/views/notice/view.php
+++ application/views/notice/view.php
@@ -1,11 +1,19 @@
 <script type="text/javascript">
 	function check() {
 		  notie.alert({
-		  type: <?php echo '"'.$type.'"'; ?>, // optional, default = 4, enum: [1, 2, 3, 4, 5, 'success', 'warning', 'error', 'info', 'neutral']
+		  type: 
+		  <?php 
+		  	if (isset($type)) {
+		  		echo '"'.$type.'"';
+		  	}else{
+		  		echo '"error"'; 
+		  	}
+		  ?>, // optional, default = 4, enum: [1, 2, 3, 4, 5, 'success', 'warning', 'error', 'info', 'neutral']
 		  text: <?php echo '"'.$message.'"'; ?>,
 		  stay: false, // optional, default = false
 		  time: 3, // optional, default = 3, minimum = 1,
 		  position: "top" // optional, default = 'top', enum: ['top', 'bottom']
 		})
 	}
+	check();
 </script>
