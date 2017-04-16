35bb7f366c9118950a47c3b8669b36336b4e3242
WangYihang <wangyihanger@gmail.com>
2017-04-16 13:58:10 +0000

    add github

--- application/views/home/view.php
+++ application/views/home/view.php
@@ -6,9 +6,11 @@
 		if you have any suggestions or comments or questions<br>
 		you can join the QQ group (514782018) to discuss<br>
 		or click the email button below the page to contact with me<br>
-		And your question will be quickly processed.
+		And your question will be quickly processed.<br>
 	<a href="https"></a>
 	<p class="lead">
-		<a href="/user/login" class="btn btn-lg btn-default">Get start</a>
+		<a href="/user/login" class="btn btn-lg btn-default">Get Start</a>
+		<h3><a target="_blank" href="https://github.com/WangYihang/SniperOJ">Fork/Star This Repositories On Github</a></h3>
+
 	</p>
 </div>
