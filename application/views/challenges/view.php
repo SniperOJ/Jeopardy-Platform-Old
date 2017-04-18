34450511480ded096922cda562676089b9e3bb7f
WangYihang <wangyihanger@gmail.com>
2017-04-18 13:45:27 +0800

    time of human

--- application/views/challenges/view.php
+++ application/views/challenges/view.php
@@ -2,6 +2,34 @@
     $this->load->language("challenges");
 ?>
 
+
+<?php
+  function formatTime($time){       
+      $rtime = date("m-d H:i",$time);       
+      $htime = date("H:i",$time);             
+      $time = time() - $time;         
+      if ($time < 60){           
+          $str = '刚刚';       
+      }elseif($time < 60 * 60){           
+          $min = floor($time/60);           
+          $str = $min.'分钟前';       
+      }elseif($time < 60 * 60 * 24){           
+          $h = floor($time/(60*60));           
+          $str = $h.'小时前 ';       
+      }elseif($time < 60 * 60 * 24 * 3){           
+          $d = floor($time/(60*60*24));           
+          if($d==1){  
+              $str = '昨天 '.$rtime;
+          }else{  
+              $str = '前天 '.$rtime;       
+          }  
+      }else{           
+          $str = $rtime;       
+      }       
+      return $str;   
+  } 
+?>
+
 <h1><?php echo $this->lang->line('CHALLENGES'); ?></h1>
 
 <div class="challenges">
@@ -28,7 +56,7 @@
           <td><?php echo $challenge_item['description']; ?></td>
           <td><?php echo $challenge_item['score']; ?></td>
           <td><?php echo $challenge_item['type']; ?></td>
-          <td><?php echo date('Y-m-d H:i:s', $challenge_item['online_time']); ?></td>
+          <td><?php echo formatTime($challenge_item['online_time']);?></td>
           <td><?php echo $challenge_item['solved_times']." / ".$challenge_item['submit_times']; ?></td>
           <?php
             echo '<td>';
