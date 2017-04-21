b84583772f7b6eba1824edfc6fb14b48c185461b
WangYihang <wangyihanger@gmail.com>
2017-04-21 11:28:08 +0800

    icon

--- application/views/navigation_bar/navigation_bar_visitor.php
+++ application/views/navigation_bar/navigation_bar_visitor.php
@@ -11,8 +11,8 @@
 			<ul class="nav navbar-nav">
 				<li><a href='/'><?php echo $this->lang->line('HOME_LABEL'); ?></a></li>
 				<li><a href='/news/view'><?php echo $this->lang->line('NEWS_LABEL'); ?></a></li>
-				<li><a href='/user/login'><?php echo $this->lang->line('LOGIN_LABEL'); ?></a></li>
-				<li><a href='/user/register'><?php echo $this->lang->line('REGISTERLABEL'); ?></a></li>
+				<li><a href='/user/login'><span class="glyphicon glyphicon-log-in"></span><?php echo $this->lang->line('LOGIN_LABEL'); ?></a></li>
+				<li><a href='/user/register'><span class="glyphicon glyphicon-user"></span><?php echo $this->lang->line('REGISTERLABEL'); ?></a></li>
 			</ul>
 		</div>
 	</div>
